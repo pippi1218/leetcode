a21ab14c8c5d838219907e0fbc4df377ab283a4c
pippi1218 <ma7pippi@gmail.com>
2022-04-19 23:34:30 +0900

    Accepted

--- probrems/java/20.valid-parentheses.java
+++ probrems/java/20.valid-parentheses.java
@@ -8,45 +8,23 @@
 class Solution {
     public boolean isValid(String s) {
 
-        String[] strs = s.split("");
+        Stack<Character> stack = new Stack<>();
 
-        int judge1, judge2, judge3;
-        judge1 = judge2 = judge3 = 0;
-
-        for (String str : strs) {
-            if (str.equals("(")) {
-                judge1++;
-            } else if (str.equals("{")) {
-                judge2++;
-            } else if (str.equals("[")) {
-                judge3++;
-            } else if (str.equals(")")) {
-                judge1--;
-            } else if (str.equals("}")) {
-                judge2--;
-            } else if (str.equals("]")) {
-                judge3--;
-            } else {
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            if (a == '(' || a == '[' || a == '{') {
+                stack.push(a);
+            } else if (stack.empty()) {
+                return false;
+            } else if (a == ')' && stack.pop() != '(') {
+                return false;
+            } else if (a == ']' && stack.pop() != '[') {
+                return false;
+            } else if (a == '}' && stack.pop() != '{') {
                 return false;
             }
         }
-        if (judge1 == 0 && judge2 == 0 && judge3 == 0) {
-            return true;
-        } else {
-            return false;
-        }
-
-    }
-
-    // 回文判定
-    static boolean isPalindrome(String s) {
-        int n = s.length();
-        for (int i = 0; i < n / 2; i++) {
-            if (s.charAt(i) != s.charAt(n - i - 1)) {
-                return false;
-            }
-        }
-        return true;
+        return stack.empty();
     }
 }
 // @lc code=end
