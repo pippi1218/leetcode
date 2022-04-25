dd8c9ef5d17f3ca34dc2d22ab92ed5ff291499d3
pippi1218 <ma7pippi@gmail.com>
2022-04-25 23:08:33 +0900

    Accepted

---
+++ probrems/java/53.maximum-subarray.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=53 lang=java
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0], maxEndingHere = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+}
+// @lc code=end
