268749a77c20c13c22db8b2ce0d1734e522016da
pippi1218 <ma7pippi@gmail.com>
2022-04-26 23:12:13 +0900

    Accepted

---
+++ probrems/java/66.plus-one.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=66 lang=java
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+
+        return newNumber;
+
+    }
+}
+// @lc code=end
